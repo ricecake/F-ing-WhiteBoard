107d4920cc27d8e649ab615d04b51f5b92847be2
Sebastian Green-Husted <ricecake@tfm.nu>
2014-08-01 18:50:25 -0400

    Update fingwb_app.erl

--- src/fingwb_app.erl
+++ src/fingwb_app.erl
@@ -10,6 +10,15 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+	    {'_', [
+			{"/", cowboy_static, {priv_file, websocket, "index.html"}},
+			{"/ws", fingwb_msg_handler, []},
+			{"/static/[...]", cowboy_static, {priv_dir, websocket, "static"}}
+		]}
+	]),
+    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]),
     fingwb_sup:start_link().
 
 stop(_State) ->
