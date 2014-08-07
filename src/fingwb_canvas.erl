59f8ee90591f485b9fbc75cea599425133130baf
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-08-07 18:43:38 -0400

    add 404 page

--- src/fingwb_canvas.erl
+++ src/fingwb_canvas.erl
@@ -6,8 +6,12 @@ init(_Type, Req, []) -> {ok, Req, undefined}.
 
 handle(Req, State) ->
 	{WhiteBoardId, Req2} = cowboy_req:binding(whiteboard_id, Req),
-	{ok, Body} = whiteboard_dtl:render([{whiteboard_id, WhiteBoardId}]),
-	{ok, Reply} = cowboy_req:reply(200, [], Body, Req2),
+	{Code, Module} = case fingwb_whiteboard:exists(WhiteBoardId) of
+		true  -> {200, whiteboard_dtl};
+		false -> {404, notFound_dtl}
+	end,
+	{ok, Body} = Module:render([{whiteboard_id, WhiteBoardId}]),
+	{ok, Reply} = cowboy_req:reply(Code, [], Body, Req2),
 	{ok, Reply, State}.
 
 terminate(_Reason, _Req, _State) -> ok.
