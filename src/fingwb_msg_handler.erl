fc3175a34a8c7575d14487903beaf8221c54e28b
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-08-08 18:32:46 -0400

    make the client handler make use of rpc system

--- src/fingwb_msg_handler.erl
+++ src/fingwb_msg_handler.erl
@@ -7,6 +7,8 @@
 -export([websocket_info/3]).
 -export([websocket_terminate/3]).
 
+-export([tell/2]).
+
 -record(ws_state, {
 	id
 }).
@@ -19,14 +21,15 @@ websocket_init(_TransportName, Req, _Opts) ->
 	case fingwb_whiteboard:exists(WhiteBoardId) of
 		false -> {shutdown, Req2};
 		true  ->
-			[ self() ! {join, Pid}|| Pid <- fingwb_whiteboard:watchers(WhiteBoardId)],
+			[ tell(self(), {join, Pid}) || Pid <- fingwb_whiteboard:watchers(WhiteBoardId)],
 			ok = fingwb_whiteboard:watch(WhiteBoardId),
 			ok = fingwb_whiteboard:notify(WhiteBoardId, {join, self()}),
-			[ self() ! Message || Message <- fingwb_whiteboard:readArchive(WhiteBoardId)],
+			[ tell(self(), Message) || Message <- fingwb_whiteboard:readArchive(WhiteBoardId)],
 			{ok, Req2, #ws_state{id=WhiteBoardId}}
 	end.
 
 websocket_handle(Data, Req, State = #ws_state{id=WbId}) ->
+	handle_client_task(Data),
 	ok = fingwb_whiteboard:publish(WbId, Data),
 	{ok, Req, State}.
 
@@ -40,3 +43,9 @@ websocket_info(Message, Req, State) ->
 websocket_terminate(_Reason, _Req, #ws_state{id=WbId}) ->
 	fingwb_whiteboard:notify(WbId, {leave, self()}),
 	fingwb_whiteboard:unWatch(WbId).
+
+handle_client_task(JSON) ->
+	{ok, Worker} = supervisor:start_child(fingwb_rpc_sup, [self()]),
+	gen_server:cast(Worker, {process, JSON}).
+
+tell(Worker, Message) -> Worker ! Message.
