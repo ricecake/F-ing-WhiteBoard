eae853dcc88e2b082846b9f9ddf1c080d31fd147
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-08-08 18:42:28 -0400

    add skeleton files for board managment processes

---
+++ src/fingwb_board_sup.erl
@@ -0,0 +1,26 @@
+-module(fingwb_board_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Args), {I, {I, start_link, Args}, permenent, 5000, worker, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(fingwb_board_srv, [])]} }.
