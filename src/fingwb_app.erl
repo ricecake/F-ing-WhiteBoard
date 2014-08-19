0619eabd4e1bc5792f6bf35e34a9bd7d0737e45e
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-08-19 19:43:21 -0400

    clean up some file locations

--- src/fingwb_app.erl
+++ src/fingwb_app.erl
@@ -18,9 +18,8 @@ start(_StartType, _StartArgs) ->
 			{"/new", fingwb_create, []},
 			{"/ws/:whiteboard_id", fingwb_msg_handler, []},
 			{"/:whiteboard_id", fingwb_canvas, []},
-			{"/js/[...]", cowboy_static, {priv_dir, fingwb, "static/js/"}},
-			{"/css/[...]", cowboy_static, {priv_dir, fingwb, "static/css/"}},
-			{"/images/[...]", cowboy_static, {priv_dir, fingwb, "static/images/"}}
+			{"/js/[...]", cowboy_static, {priv_dir, fingwb, "js/"}},
+			{"/css/[...]", cowboy_static, {priv_dir, fingwb, "css/"}}
 		]}
 	]),
 	{ok, _} = cowboy:start_http(http, 25, [{ip, {127,0,0,1}}, {port, 8080}],
